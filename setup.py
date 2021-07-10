b59d19842082147a5187fa92f9c44b45d147adc7
kiwislob <kiwislob@gmail.com>
2021-07-10 23:03:12 +1200

    adding typed indicator

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ with open("README.md") as h:
     desc = h.read()
 
 setup(
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
     name = "to",
-    version="0.0.1",
     description="Automatic type conversion framework",
     long_description=desc,
     author="Jason Dixon",
     author_email="jason.dixon.email@gmail.com",
     url="https://github.com/internetimagery/to",
     packages=["to"],
+    package_data={"to": ["py.typed"]},
 )
